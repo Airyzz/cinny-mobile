4722b45cb2c2c4f1497cf7ef38e10c73b9060603
Airyz <36567925+Airyzz@users.noreply.github.com>
2023-01-17 22:07:53 +1030

    Add generated source + changes

---
+++ src-tauri/gen/android/app/app/src/main/java/xyz/airyz/cinny/app/MainActivity.kt
@@ -0,0 +1,13 @@
+package xyz.airyz.cinny.app
+
+import android.annotation.SuppressLint
+import android.os.Build
+import android.os.Bundle
+
+class MainActivity : TauriActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getSupportActionBar()?.hide()
+        println("Hello, World!")
+    }
+}
