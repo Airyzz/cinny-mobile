cd15ab193dfb1b2c4be10efe1abce211d7c53a5b
Airyz <36567925+Airyzz@users.noreply.github.com>
2023-01-17 23:07:12 +1030

    Remove action bar

--- src-tauri/gen/android/app/app/src/main/java/xyz/airyz/cinny/app/MainActivity.kt
+++ src-tauri/gen/android/app/app/src/main/java/xyz/airyz/cinny/app/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 class MainActivity : TauriActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getSupportActionBar()?.hide()
         println("Hello, World!")
     }
 }
